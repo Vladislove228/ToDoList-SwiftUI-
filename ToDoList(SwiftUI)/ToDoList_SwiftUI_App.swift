11b4c453d664fee5ae83f76d930446fe5a8b492a
Vladislove228 <wlad.151@mail.ru>
2023-10-30 11:11:35 +0100

    1

---
+++ ToDoList(SwiftUI)/ToDoList_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUI_App.swift
+//  ToDoList(SwiftUI)
+//
+//  Created by Владислав Квинто on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
